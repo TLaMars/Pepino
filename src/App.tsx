9c99a28d3629f19c66fed6af7ff3de0d7157101f
Thomas Lamars <thomas.lamars@wearetriple.com>
2025-02-16 10:50:47 +0100

    fix(tauri): don't render dragbar on non tauri apps

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import { loadingAtom } from "src/store/control-settings";
 import Window from "src/components/Window/Window";
 import cx from "src/utils/classnames";
 import styles from "./App.module.scss";
+import { isTauri } from "@tauri-apps/api/core";
 
 const Pepino: React.FC = () => {
   const isLoading = useAtomValue(loadingAtom);
 
   return (
     <div className={cx(styles.container, isLoading && styles.loading)}>
-      <div data-tauri-drag-region className={styles.tauri}></div>
+      {isTauri() && <div data-tauri-drag-region className={styles.tauri}></div>}
       <div className={styles.windowContainer}>
         <Window />
       </div>
