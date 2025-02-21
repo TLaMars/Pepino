ff8fe372a2a31dbb5cb19e3778cd5c16b7c0b510
Thomas Lamars <thomas.lamars@wearetriple.com>
2025-02-21 16:43:04 +0100

    chore(updater): remove update status

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,17 @@ import ControlBar from "src/components/ControlBar/ControlBar";
 import Frame from "src/components/Frame/Frame";
 import { loadingAtom } from "src/store/control-settings";
 import cx from "src/utils/classnames";
-import appUpdater, { AppUpdaterEvent } from "./utils/tauri/app-updater";
+import type { AppUpdaterEvent } from "./utils/tauri/app-updater";
+import appUpdater from "./utils/tauri/app-updater";
 
 import styles from "./App.module.scss";
 
 const Pepino: React.FC = () => {
   const isLoading = useAtomValue(loadingAtom);
-  const [updaterResult, setUpdaterResult] = useState<AppUpdaterEvent>();
+  const [, setUpdaterResult] = useState<AppUpdaterEvent>();
 
   useEffect(() => {
+    // Tauri app updater
     const getUpdateResult = async () => {
       const result = await appUpdater();
       setUpdaterResult(result);
@@ -23,22 +25,13 @@ const Pepino: React.FC = () => {
   }, []);
 
   return (
-    <>
-      <div className={cx(styles.container, isLoading && styles.loading)}>
-        {isTauri() && (
-          <div data-tauri-drag-region className={styles.tauri}></div>
-        )}
-        <div className={styles.frameSpacing}>
-          <Frame />
-        </div>
-        <ControlBar />
+    <div className={cx(styles.container, isLoading && styles.loading)}>
+      {isTauri() && <div data-tauri-drag-region className={styles.tauri}></div>}
+      <div className={styles.frameSpacing}>
+        <Frame />
       </div>
-      {updaterResult === AppUpdaterEvent.Finished && <div>Update Finished</div>}
-      {updaterResult === AppUpdaterEvent.Failed && <div>Update Failed</div>}
-      {updaterResult === AppUpdaterEvent.NoUpdate && (
-        <div>No Update Available</div>
-      )}
-    </>
+      <ControlBar />
+    </div>
   );
 };
 
